26c3c3027088c2df6ed2497ab1a7684bc2d4d51a
Jeff-desv <brunojeff0012@gmail.com>
2024-06-16 17:58:21 -0300

    modificacoes

---
+++ minha-primeira-web-api-spring/src/main/java/web_api/controller/UsuarioController.java
@@ -0,0 +1,25 @@
+package web_api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import web_api.model.Usuario;
+import web_api.repository.UserRepository;
+
+@RestController
+public class UsuarioController {
+
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping("/users")
+	public List<Usuario>getUsers(){
+		return repository.findAll();
+	}
+	@GetMapping("/users/{username}")
+	public Usuario getOne(String username) {
+		return repository.findByUsername(username);
+	}
+}
