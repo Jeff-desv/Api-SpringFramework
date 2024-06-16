7f1bf6ab4fd09e5b04219c8f0bf1b5c6f1192a71
Jeff-desv <brunojeff0012@gmail.com>
2024-06-16 14:59:11 -0300

    criando o Model

---
+++ minha-primeira-web-api-spring/src/main/java/web_api/model/Usuario.java
@@ -0,0 +1,23 @@
+package web_api.model;
+
+public class Usuario {
+	
+	private Integer id;
+	private String login;
+	private String password;
+	
+	public Usuario() {}
+	
+	public Usuario(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "User{" +
+			   "login='" + login + '\'' +
+			   ", password='" + password + '\'' +
+			   '}';
+	}
+}
