9579cd9fd5fe8dc3a0a7c0ff0ce4eb5802818d2e
Jeff-desv <brunojeff0012@gmail.com>
2024-06-16 18:22:17 -0300

    Documentando API

--- minha-primeira-web-api-spring/src/main/java/web_api/controller/UsuarioController.java
+++ minha-primeira-web-api-spring/src/main/java/web_api/controller/UsuarioController.java
@@ -3,7 +3,11 @@ package web_api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import web_api.model.Usuario;
 import web_api.repository.UserRepository;
@@ -21,5 +25,13 @@ public class UsuarioController {
 	@GetMapping("/users/{username}")
 	public Usuario getOne(String username) {
 		return repository.findByUsername(username);
+	}
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable("id")Integer id) {
+		repository.deleteById(id);
+	}
+	@PostMapping("/users")
+	public void pstUser(@RequestBody Usuario usuario) {
+		repository.save(usuario);
 	}
 }
