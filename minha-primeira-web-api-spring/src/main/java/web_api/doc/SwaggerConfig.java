a6bd3b3eeaed9473f9e2da7d1a7ba2a4163b43f8
Jeff-desv <brunojeff0012@gmail.com>
2024-06-16 18:46:45 -0300

    Documentando a API

--- minha-primeira-web-api-spring/src/main/java/web_api/doc/SwaggerConfig.java
+++ minha-primeira-web-api-spring/src/main/java/web_api/doc/SwaggerConfig.java
@@ -1,9 +1,17 @@
 package web_api.doc;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -31,5 +39,23 @@ public class SwaggerConfig {
 		apiInfoBuilder.contact(this.contato());
 		
 		return apiInfoBuilder;
+	}
+	
+	@Bean
+	public Docket detalheApi() {
+		
+		Docket docket = new Docket(DocumentationType.SWAGGER_2);
+		
+		docket 
+			  
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("pacote.comseus.controllers"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(this.informacoesApi().build())
+				.consumes(new HashSet<String>(Arrays.asList("appLication/jason")))
+				.produces(new HashSet<String>(Arrays.asList("applicatiom/json")));
+		
+		return docket;
 	}
 }
