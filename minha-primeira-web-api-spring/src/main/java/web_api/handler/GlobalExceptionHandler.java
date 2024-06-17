d90b611a1fd6b2130dcc8d4b217ca7665adfac4e
Jeff-desv <brunojeff0012@gmail.com>
2024-06-17 20:09:04 -0300

    Atualizando o Handler

--- minha-primeira-web-api-spring/src/main/java/web_api/handler/GlobalExceptionHandler.java
+++ minha-primeira-web-api-spring/src/main/java/web_api/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package web_api.handler;
 
+import org.springframework.cglib.proxy.UndeclaredThrowableException;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import jakarta.annotation.Resource;
 
 
@@ -24,6 +28,32 @@ public class GlobalExceptionHandler {
 	
 	private ResponseError responseError(String message, HttpStatus statusCode) {
 		ResponseError responseError = new ResponseError();
+		responseError.setStatus("error");
+		responseError.setError(message);
+		responseError.setStatusCode(statusCode.value());
 		return responseError;
+	}
+	
+	@ExceptionHandler(Exception.class)
+	private ResponseEntity<Object> handlerGeneral(Exception e, WebRequest request){
+		if(e.getClass().isAssignableFrom(UndeclaredThrowableException.class)) {
+			UndeclaredThrowableException exception = (UndeclaredThrowableException) e;
+			return handleBusinessException((BusinessException) exception.getUndeclaredThrowable(), request);
+		} else {
+			String message = messageSource.getMessage("error.server", new Object[] {e.getMessage()}, null);
+			ResponseError error = responseError(message, HttpStatus.INTERNAL_SERVER_ERROR);
+			return handleExceptionInternal(e, error, headers(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+		}
+	}
+
+	private ResponseEntity<Object> handleExceptionInternal(Exception e, ResponseError error, HttpHeaders headers,
+			HttpStatus internalServerError, WebRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ResponseEntity<Object> handleBusinessException(BusinessException undeclaredThrowable, WebRequest request) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
